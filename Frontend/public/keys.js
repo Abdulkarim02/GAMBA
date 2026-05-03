31661eac72e3649e313fe71c362a5179c306374b
abdulelah almanea <a.almanea922@gmail.com>
2026-05-03 16:50:36 +0300

    Delete Frontend/public/keys.js

--- Frontend/public/keys.js
+++
@@ -1,6 +0,0 @@
-const KEY_CONTENT_LLM = 'qE3vbduF0m7Sz1uvpcHs';
-const KEY_LLM         = '72pUbskeYMjt7QHODt8JD4mZknVmIoxq';
-const KEY_SERPAPI      = 'ee7037a7f31c07b8e2ecfa6b276dd9f5baf8cba72136b0a6f3f45613c28f3502';
-const KEY_TMDB         = '0408161fb7ac27f4d0e21cd93431cc3f';
-const KEY_SAFEBROWSING = '';
-const KEY_SEC_LLM      = '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';
